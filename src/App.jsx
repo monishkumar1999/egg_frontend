9dc0d0607ba24766a42ba4476ce6896c0bd99949
monishkumar1999 <monishkumarapj@gmail.com>
2024-12-10 17:13:41 +0530

    Fix line endings

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white text-4xl h-screen flex items-center justify-center">
+      Hello, Tailwind!
+    </div>
+  );
+}
+
+export default App
